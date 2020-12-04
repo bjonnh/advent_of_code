7c460035919d87f156076128e9249ee83acde7bb
Jonathan Bisson <research@bjonnh.net>
2020-12-04 00:36:48 -0600

    make it more fun(ctional)

--- src/main/kotlin/y2020/day04/main.kt
+++ src/main/kotlin/y2020/day04/main.kt
@@ -1,3 +1,5 @@
+// Today is ugly little functional version day
+
 package y2020.day04
 
 import java.io.File
@@ -9,34 +11,19 @@ infix fun String?.inRange(v: Pair<Int, Int>): Boolean = this?.toIntOrNull() in v
 fun String?.rFindGroups(r: String) = r.toRegex().find(this ?: "")?.groupValues
 fun String?.rMatches(r: String) = (this?.matches(r.toRegex()) == true)
 
-fun count(data: List<String>): Pair<Int, Int> {
-    var (validPassports, validPassports2) = Pair(0, 0)
-
-    data.forEach { it ->
-        val pMap = it.split(" ", "\n").filter { it.isNotBlank() }.map {
+fun main() {
+    val text = File("data/2020/day04/input.txt").readText().split("\n\n")
+    text.map { it ->
+        it.split(" ", "\n").filter { it.isNotBlank() }.map {
             it.split(":").let { el -> el[0] to el[1] }
         }.toMap()
-
-        if (pMap.keys.containsAll(required)) {
-            validPassports += 1
-            if ((pMap["byr"] inRange (1920 to 2002)) && (pMap["iyr"] inRange (2010 to 2020)) &&
+    }.filter { it.keys.containsAll(required) }.also { println("Part1: ${it.size}") }.filter { pMap ->
+        (pMap["byr"] inRange (1920 to 2002)) && (pMap["iyr"] inRange (2010 to 2020)) &&
                 (pMap["eyr"] inRange (2020 to 2030)) &&
                 (pMap["hgt"].rFindGroups("([0-9]+(?=in))|([0-9]+(?=cm))")?.let {
                     (it[1] inRange (59 to 76)) || (it[2] inRange (150 to 193))
                 } == true) &&
                 (pMap["hcl"].rMatches("#[0-9a-f]{6}")) && (pMap["ecl"] in validEyes) &&
                 (pMap["pid"].rMatches("[0-9]{9}"))
-            ) {
-                validPassports2 += 1
-            }
-        }
-    }
-    return Pair(validPassports, validPassports2)
-}
-
-fun main() {
-    val file = File("data/2020/day04/input.txt")
-    val text = file.readText().split("\n\n")
-    val parts = count(text)
-    println("Parts result is $parts")
+    }.let { println("Part2: ${it.size}") }
 }
