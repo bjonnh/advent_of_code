53e12ea9fee76177aa7d375d34e1b95838856422
Jonathan Bisson <research@bjonnh.net>
2020-12-09 09:40:12 -0600

    2020 day 9 shifting windows

---
+++ src/main/kotlin/y2020/day09/main.kt
@@ -0,0 +1,25 @@
+package y2020.day09
+
+import helpers.linesFile
+
+fun main() {
+    val lines = linesFile("data/2020/09_input.txt").map { it.toLong() }
+    (25 until lines.size).map { index ->
+        val setOfNumbersPrevious = lines.subList(index - 25, index).toSet().toList()
+        val valid = (setOfNumbersPrevious.indices).any { i ->
+            (i until setOfNumbersPrevious.size).any { j ->
+                (setOfNumbersPrevious[i] + setOfNumbersPrevious[j]) == lines[index]
+            }
+        }
+        if (!valid) {
+            println("Part 1: ${lines[index]}")
+            (lines.indices).map { i ->
+                (2 until lines.size - i).forEach { j ->
+                    val arr = lines.subList(i, i + j)
+                    if (arr.sum() == lines[index]) println("Found the range $i $j part2 result is ${(arr.minOrNull() ?: 0) + (arr.maxOrNull() ?: 0)}")
+                    if (arr.sum() > lines[index]) return@forEach
+                }
+            }
+        }
+    }
+}
