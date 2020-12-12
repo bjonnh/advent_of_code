15987b51509463b6f4bb1623d65efc5634995959
Jonathan Bisson <research@bjonnh.net>
2020-12-12 15:26:32 -0600

    2020 day 12 weird boats now without mussels

--- src/main/kotlin/y2020/day12/main.kt
+++ src/main/kotlin/y2020/day12/main.kt
@@ -1,40 +1,24 @@
 package y2020.day12
 
 import helpers.linesFile
+import kotlin.math.abs
 
-interface ShipActions {
+interface Ship {
     var latitude: Int
     var longitude: Int
     fun north(v: Int)
-    fun south(v: Int)
-    fun west(v: Int)
     fun east(v: Int)
-    fun left(v: Int)
     fun right(v: Int)
     fun forward(v: Int)
-
-    fun distance() = Math.abs(latitude) + Math.abs(longitude)
+    fun distance() = abs(latitude) + abs(longitude)
 }
 
-data class Ship(
-    var facing: Int = 1 // East
-) : ShipActions {
-    override var latitude: Int = 0
-    override var longitude: Int = 0
-
+data class Ship1(var facing: Int = 1, override var latitude: Int = 0, override var longitude: Int = 0) : Ship {
     override fun north(v: Int) {
-        latitude -= v
-    }
-
-    override fun south(v: Int) {
         latitude += v
     }
 
     override fun east(v: Int) {
-        longitude -= v
-    }
-
-    override fun west(v: Int) {
         longitude += v
     }
 
@@ -42,28 +26,19 @@ data class Ship(
         facing = Math.floorMod((facing + shift), 4)
     }
 
-    override fun left(v: Int) {
-        rot(-v / 90)
-    }
-
-    override fun right(v: Int) {
-        rot(v / 90)
-    }
+    override fun right(v: Int) = rot(v / 90)
 
     override fun forward(v: Int) {
         when (facing) {
             0 -> north(v)
-            2 -> south(v)
-            3 -> west(v)
+            2 -> north(-v)
+            3 -> east(-v)
             1 -> east(v)
         }
     }
 }
 
-data class Ship2(var wpE: Int, var wpN: Int) : ShipActions {
-    override var latitude: Int = 0
-    override var longitude: Int = 0
-
+data class Ship2(var wpE: Int, var wpN: Int, override var latitude: Int = 0, override var longitude: Int = 0) : Ship {
     override fun forward(v: Int) {
         latitude += v * wpN
         longitude += v * wpE
@@ -73,53 +48,30 @@ data class Ship2(var wpE: Int, var wpN: Int) : ShipActions {
         wpN += v
     }
 
-    override fun south(v: Int) {
-        wpN -= v
-    }
-
     override fun east(v: Int) {
         wpE += v
     }
 
-    override fun west(v: Int) {
-        wpE -= v
-    }
-
-    override fun right(v: Int) {
-        repeat(v / 90) {
-            val s = wpE
-            wpE = wpN
-            wpN = -s
-        }
-    }
-
-    override fun left(v: Int) {
-        repeat(v / 90) {
-            val s = wpE
-            wpE = -wpN
-            wpN = s
-        }
-    }
+    override fun right(v: Int) = repeat(Math.floorMod(v / 90, 360)) { wpE = wpN.also { wpN = -wpE } }
 }
 
 fun main() {
     val orders = linesFile("data/2020/12_input.txt")
-    val ship1 = Ship()
+    val ship1 = Ship1()
     val ship2 = Ship2(10, 1)
-    listOf(ship1, ship2).forEach { ship ->
-        orders.forEach {
-            val v = it.substring(1).toInt()
+    orders.forEach {
+        val v = it.substring(1).toInt()
+        listOf(ship1, ship2).forEach { ship ->
             when (it[0]) {
                 'N' -> ship.north(v)
-                'S' -> ship.south(v)
+                'S' -> ship.north(-v)
                 'E' -> ship.east(v)
-                'W' -> ship.west(v)
-                'L' -> ship.left(v)
+                'W' -> ship.east(-v)
+                'L' -> ship.right(-v)
                 'R' -> ship.right(v)
                 'F' -> ship.forward(v)
             }
         }
     }
-    println(ship1.distance())
-    println(ship2.distance())
+    println("Part1 ${ship1.distance()} Part2 ${ship2.distance()}")
 }
