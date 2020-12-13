62e6017cac0f5b431e144f0c19c669b885ada07d
Jonathan Bisson <research@bjonnh.net>
2020-12-13 00:50:09 -0600

    2020 day 13 better way to do part 1

--- src/main/kotlin/y2020/day13/main.kt
+++ src/main/kotlin/y2020/day13/main.kt
@@ -7,23 +7,16 @@ fun main() {
     val timeStamp = lines[0].toLong()
     val stuff = lines[1].split(",").filter { it != "x" }.map { it.toLong() }
 
-    var time = timeStamp
-    while (true) {
-        val out = stuff.firstOrNull { time % it == 0L }
-        if (out != null) {
-            println(out * (time - timeStamp))
-            break
-        }
-        time++
-    }
+    val part1 = stuff.map { it to it - (timeStamp % it) }.minByOrNull { it.second }?.let { it.first * it.second }
+    println("Part1: $part1")
 
-    time = 100000000000000L
+    var time = 100000000000000L
     val stuff2 = lines[1].split(",").mapIndexed { idx, it -> it to idx }.filter { it.first != "x" }
         .map { it.first.toLong() to it.second.toLong() }
     var step: Long
     var count = 0
     while (true) {
-        val vals = stuff2.filter { (it, idx) -> (time + idx) % (it) == 0L }.map {it.first}
+        val vals = stuff2.filter { (it, idx) -> (time + idx) % (it) == 0L }.map { it.first }
         step = vals.fold(1L) { acc, value -> acc * value }
         if (vals.size == stuff2.size) {
             break
