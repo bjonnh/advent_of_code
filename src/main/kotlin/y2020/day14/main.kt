ccd7332993426469772d755dbd0679e33ad74fb5
Jonathan Bisson <research@bjonnh.net>
2020-12-13 23:38:16 -0600

    2020 day 14 cleaning up

--- src/main/kotlin/y2020/day14/main.kt
+++ src/main/kotlin/y2020/day14/main.kt
@@ -1,33 +1,11 @@
 package y2020.day14
 
 import helpers.linesFile
-import java.lang.Math.pow
-
+import kotlin.math.pow
 
 fun main() {
     val currentMask = List(36) { '0' }.toMutableList()
     val memory = mutableMapOf<Int, Long>()
-    linesFile("data/2020/14_input.txt").map {
-        val (command, value) = it.split(" = ")
-        when (command) {
-            "mask" -> value.mapIndexed { idx, it -> currentMask[idx] = it }
-            else -> {
-                val valueLong = value.toLong().toString(2).padStart(36, '0')
-                val newVal = valueLong.mapIndexed { idx, it ->
-                    when (currentMask[idx]) {
-                        'X' -> it
-                        '1' -> '1'
-                        '0' -> '0'
-                        else -> throw RuntimeException("Unknown symbol in $currentMask")
-                    }
-                }.joinToString("").toLong(2)
-                val address = command.substring(4).trimEnd(']').toInt()
-                memory[address] = newVal
-            }
-        }
-    }
-    println(memory.values.sum())
-
     val memory2 = mutableMapOf<Long, Long>()
     linesFile("data/2020/14_input.txt").map {
         val (command, value) = it.split(" = ")
@@ -35,21 +13,29 @@ fun main() {
             "mask" -> value.mapIndexed { idx, it -> currentMask[idx] = it }
             else -> {
                 val valueLong = value.toLong()
+                val address = command.substring(4).trimEnd(']').toInt()
+
+                val valPart1 = valueLong.toString(2).padStart(36, '0').mapIndexed { idx, it ->
+                    when (val msk = currentMask[idx]) {
+                        'X' -> it
+                        else -> msk
+                    }
+                }.joinToString("").toLong(2)
+
+                memory[address] = valPart1
+
                 val numberofX = currentMask.count { it == 'X' }
-                val vals = (0 until pow(2.0, numberofX.toDouble()).toInt()).map {
+                val addressPart2 = address.toString(2).padStart(36, '0')
+
+                (0 until 2.0.pow(numberofX.toDouble()).toInt()).map { // Generate all the values that will replace the X
                     it.toString(2).padStart(numberofX, '0')
-                }
-                val address = command.substring(4).trimEnd(']').toInt().toString(2).padStart(36, '0')
-                vals.forEach { newVals ->
+                }.forEach { newVals ->  // And we then replace the X with these
                     var count = 0
                     val addr = currentMask.mapIndexed { idx, it ->
                         when (it) {
-                            '0' -> address[idx]
+                            '0' -> addressPart2[idx]
                             '1' -> '1'
-                            'X' -> {
-                                newVals[count].also { count++ }
-                            }
-                            else -> throw RuntimeException("Darn")
+                            else -> newVals[count].also { count++ }
                         }
                     }.joinToString("").toLong(2)
                     memory2[addr] = valueLong
@@ -57,5 +43,6 @@ fun main() {
             }
         }
     }
+    println(memory.values.sum())
     println(memory2.values.sum())
 }
