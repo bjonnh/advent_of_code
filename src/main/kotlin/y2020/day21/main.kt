d88f94b4f05139bf2f56b8f2e105465ca95e5d2a
Jonathan Bisson <research@bjonnh.net>
2020-12-25 22:05:15 -0600

    2020 day 21 cleanup

--- src/main/kotlin/y2020/day21/main.kt
+++ src/main/kotlin/y2020/day21/main.kt
@@ -3,17 +3,13 @@ package y2020.day21
 import java.io.File
 
 fun main() {
-    val lines = File("data/2020/21_input.txt").readLines()
-    val input = lines.map {
+    val input = File("data/2020/21_input.txt").readLines().map {
         val (ingreRaw, allergRaw) = it.trim(')').split(" (contains ")
         Pair(ingreRaw.split(" "), allergRaw.split(", "))
     }
-    println(input)
+
     val ingredients = input.flatMap { it.first }.toSet()
     val allergens = input.flatMap { it.second }.toSet()
-
-    println(ingredients)
-    println(allergens)
 
     val allergensToIngredients = allergens.map { allergen ->
         allergen to ingredients.toSet().toMutableSet()
@@ -21,25 +17,17 @@ fun main() {
 
     val finishedIngredients = mutableSetOf<String>()
 
-    // First pass take all the allergens that have >1 recipe, and find the intersection
     while (finishedIngredients.size != allergens.size) {
-        val matches = allergensToIngredients.filter { it.value.size > 1 }.map { (allergen, content) ->
-
+        allergensToIngredients.filter { it.value.size > 1 }.map { (allergen, _) ->
             allergen to input.filter { it.second.contains(allergen) }
                 .map { it.first.filterNot { it in finishedIngredients } }
-        }.map {
-            it.first to it.second.reduce { acc, list -> acc.toSet().intersect(list.toSet()).toList() }
-        }
-        matches.filter {
-            it.second.size == 1
-        }.forEach { matching ->
-            allergensToIngredients[matching.first]?.retainAll(matching.second)
-            allergensToIngredients.filter { it.key != matching.first }
-                .forEach { it.value.remove(matching.second.first()) }
-        }
+        }.map { it.first to it.second.reduce { acc, list -> acc.toSet().intersect(list.toSet()).toList() } }
+            .filter { it.second.size == 1 }.forEach { (first, second) ->
+                allergensToIngredients[first]?.retainAll(second)
+                allergensToIngredients.filterNot { it.key == first }.forEach { it.value.remove(second.first()) }
+            }
         finishedIngredients.addAll(allergensToIngredients.filter { it.value.size == 1 }.map { it.value.first() })
     }
     println(input.map { it.first.count { it !in finishedIngredients } }.sum())
-
-    println(allergensToIngredients.toSortedMap().map { it.value.first() }.joinToString(",") )
+    println(allergensToIngredients.toSortedMap().map { it.value.first() }.joinToString(","))
 }
